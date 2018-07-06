02a4298057b2df0d99845a2be917a3bd9f701cd3
Andre Odendaal <aodendaal@gmail.com>
2018-07-06 16:39:35 +0200

    Initial Commit

---
+++ Abp.AppFactory.Interfaces/IBlobStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Abp.AppFactory.Interfaces
+{
+    public interface IBlobStorage
+    {
+        string Endpoint { get; }
+        Task UploadAsync(string containerName, string filename, byte[] file);
+        Task UploadAsync(string containerName, string directory, string filename, byte[] file);
+    }
+}
---
+++ Abp.AppFactory.Interfaces/IExcelGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Abp.AppFactory.Interfaces
+{
+    public interface IExcelGenerator 
+    {
+        Task<string> CreateAndStoreAsync<T>(IEnumerable<T> content, string filename = "export.xlsx", string worksheetTitle = "Sheet1");
+        Task<string> CreateAndStoreAsync<T>(IEnumerable<T> content, string[] headings, string filename = "export.xlsx", string worksheetTitle = "Sheet1");
+        Task<string> CreateAndStoreAsync<T>(IEnumerable<T> content, IBlobStorage blobStorage, string containerName, string directory = null, string filename = "export.xlsx", string worksheetTitle = "Sheet1");
+        Task<string> CreateAndStoreAsync<T>(IEnumerable<T> content, string[] headings, IBlobStorage blobStorage, string containerName, string directory = null, string filename = "export.xlsx", string worksheetTitle = "Sheet1");
+
+        Task<byte[]> CreateAsync<T>(IEnumerable<T> content, string worksheetTitle = "Sheet1");
+        Task<byte[]> CreateAsync<T>(IEnumerable<T> content, string[] headings, string worksheetTitle = "Sheet1");
+
+        void SetStore(IBlobStorage blobStorage, string containerName, string directory = null);
+    }
+}
