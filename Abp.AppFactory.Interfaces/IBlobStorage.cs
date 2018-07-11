b4b3d87646cd9a10476923b223cccdb5c53c4e6f
Andre Odendaal <aodendaal@gmail.com>
2018-07-11 16:13:29 +0200

    Change UploadAsync() to return the URL of the uploaded blob

--- Abp.AppFactory.Interfaces/IBlobStorage.cs
+++ Abp.AppFactory.Interfaces/IBlobStorage.cs
@@ -5,8 +5,8 @@ namespace Abp.AppFactory.Interfaces
     public interface IBlobStorage
     {
         string Endpoint { get; }
-        Task UploadAsync(string containerName, string filename, byte[] file);
-        Task UploadAsync(string containerName, string directory, string filename, byte[] file);
+        Task<string> UploadAsync(string containerName, string filename, byte[] file);
+        Task<string> UploadAsync(string containerName, string directory, string filename, byte[] file);
         Task DeleteAsync(string containerName, string directory, string filename);
     }
 }
