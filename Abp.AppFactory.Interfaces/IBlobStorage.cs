14aedee2c87920ce44274f55bf1885aec5a7ca13
Andre Odendaal <aodendaal@gmail.com>
2018-07-11 16:07:08 +0200

    Add delete function to IBlobStorage

--- Abp.AppFactory.Interfaces/IBlobStorage.cs
+++ Abp.AppFactory.Interfaces/IBlobStorage.cs
@@ -7,5 +7,6 @@ namespace Abp.AppFactory.Interfaces
         string Endpoint { get; }
         Task UploadAsync(string containerName, string filename, byte[] file);
         Task UploadAsync(string containerName, string directory, string filename, byte[] file);
+        Task DeleteAsync(string containerName, string directory, string filename);
     }
 }
