e2de362aabfc44888e52900a19b48f4793115f91
Marc Raftesath (Manpower SA (Pty) Ltd) <Marcraffy@gmail.com>
2018-09-10 13:26:09 +0200

    Add IEmail and ISendGridResponse

---
+++ Abp.AppFactory.Interfaces/IEmail.cs
@@ -0,0 +1,13 @@
+﻿namespace Abp.AppFactory.Interfaces
+{
+    public interface IEmail
+    {
+        string SenderEmailAddress { get; set; }
+        string SenderName { get; set; }
+        string SubjectContent { get; set; }
+        string BodyTextContent { get; set; }
+        string BodyHtmlContent { get; set; }
+        string RecepientEmailAddress { get; set; }
+        string RecepientName { get; set; }
+    }
+}
---
+++ Abp.AppFactory.Interfaces/ISendGridResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Abp.AppFactory.Interfaces
+{
+    public interface ISendGridResponse
+    {
+        HttpStatusCode StatusCode { get; set; }
+        HttpContent Body { get; set; }
+        HttpResponseHeaders Headers { get; set; }
+    }
+}
