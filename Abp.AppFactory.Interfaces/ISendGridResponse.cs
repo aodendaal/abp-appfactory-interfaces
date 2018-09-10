8e7ac7a9ddbfa9f2203a27df975439d294b8cb74
Marc Raftesath (Manpower SA (Pty) Ltd) <Marcraffy@gmail.com>
2018-09-10 15:07:20 +0200

    Add DeserializedResponses to ISendGridResponse

--- Abp.AppFactory.Interfaces/ISendGridResponse.cs
+++ Abp.AppFactory.Interfaces/ISendGridResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
@@ -9,5 +10,7 @@ namespace Abp.AppFactory.Interfaces
         HttpStatusCode StatusCode { get; set; }
         HttpContent Body { get; set; }
         HttpResponseHeaders Headers { get; set; }
+        Dictionary<string, dynamic> DeserializedResponseBody { get; set; }
+        Dictionary<string, string> DeserializedResponseHeaders { get; set; }
     }
 }
